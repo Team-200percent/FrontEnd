a1e39895bafb1da2c9e8bfe887ffae7e8baed279
Benzity <benzity@naver.com>
2025-08-09 02:10:18 +0900

    feat: add basic scaffold for Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
 import Explore from "./pages/Explore";
 import MyPage from "./pages/MyPage";
+import Layout from "./layout/Layout";
+import NavBar from "./components/NavBar";
 
-function App() {
+function AppInner() {
+  const location = useLocation(); // 현재 URL 전체 정보를 주는 React Hook
+  const hideNavPaths = ["/login", "/onboarding"]; // 여기에 숨길 경로들 적기
+  const shouldHideNav = hideNavPaths.some(path => location.pathname.startsWith(path));
+  // 현재 경로가 hideNavPaths 중 하나로 시작하는지 확인하는 boolean 변수 
+
   return (
-    <BrowserRouter>
+    <Layout>
       <div style={{ paddingBottom: "80px" }}>
         <Routes>
-          
           {/* Main Pages */}
           <Route path="/" element={<Home />} />
           <Route path="/map" element={<Map />} />
@@ -19,9 +25,16 @@ function App() {
 
           {/* Sub Pages */}
         </Routes>
+        {!shouldHideNav && <NavBar />}
       </div>
+    </Layout>
+  )
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppInner />
     </BrowserRouter>
   );
 }
-
-export default App;
