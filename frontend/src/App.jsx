14657c45f2a1c9eb1c15c55962848c6334dd18b3
Benzity <benzity@naver.com>
2025-08-09 01:28:39 +0900

    feat: 초기 구조 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Map from "./pages/Map";
+import Explore from "./pages/Explore";
+import MyPage from "./pages/MyPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div style={{ paddingBottom: "80px" }}>
+        <Routes>
+          
+          {/* Main Pages */}
+          <Route path="/" element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/mypage" element={<MyPage />} />
+
+          {/* Sub Pages */}
+        </Routes>
       </div>
-      <h1>이것은 테스트입니다.</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
