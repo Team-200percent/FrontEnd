a310198e97b25e357d88c1e19ba7fbc8f933a470
Benzity <benzity@naver.com>
2025-08-12 04:56:09 +0900

    chore: install svgr plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 // vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], [svgr()],
   server: {
     host: true, // 기본적으로 'localhost'인데 true로 하면 모든 IP에서 접근 가능
     port: 5173, // 기본 포트. 필요하면 다른 포트로 변경 가능
   },
 });
+
