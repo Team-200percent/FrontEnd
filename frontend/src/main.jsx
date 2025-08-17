9418aab8bba036ccc34cb8492963cb1b3aead7a6
Benzity <benzity@naver.com>
2025-08-18 07:40:43 +0900

    add: custom api const

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import GlobalStyle from './styles/GlobalStyle.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyle from "./styles/GlobalStyle.jsx";
+import axios from "axios";
 
-createRoot(document.getElementById('root')).render(
+export const api = axios.create({
+  baseURL: "https://200percent.p-e.kr",
+  withCredentials: true, //
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyle />
     <App />
   </StrictMode>
-)
+);
