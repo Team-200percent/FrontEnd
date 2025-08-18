09f7742e356b27ef4bcd7910d0d437916852d701
Benzity <benzity@naver.com>
2025-08-19 06:30:53 +0900

    add: frontend svg, png data

---
+++ frontend/src/data/HomeData.js
@@ -0,0 +1,74 @@
+export const LEVEL_META = {
+  1: {
+    title: "동네 초보",
+    subtitle: "필수 행정 / 기본 생활 인프라를 파악해보세요",
+    headerImage: "/icons/home/levelheader/level1.png",
+  },
+  2: {
+    title: "동네 적응자",
+    subtitle: "생활 편의를 확장해보세요",
+    headerImage: "/icons/home/levelheader/level2.png",
+  },
+  3: {
+    title: "동네 탐험가",
+    subtitle: "다양한 상권과 문화시설을 경험해보세요",
+    headerImage: "/icons/home/levelheader/level3.png",
+  },
+  4: {
+    title: "동네 단골",
+    subtitle: "나만의 단골 가게를 찾아보세요",
+    headerImage: "/icons/home/levelheader/level4.png",
+  },
+  5: {
+    title: "동네 고수",
+    subtitle: "정착을 끝내고 토박이처럼 생활해보세요",
+    headerImage: "/icons/home/levelheader/level5.png",
+  },
+};
+
+export const CATEGORY_ICONS = {
+  heart: {
+    active_unpressed: "/icons/home/levelicon/heart/active.png",
+    active_pressed: "/icons/home/levelicon/heart/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/heart/inactive.png",
+    inactive_pressed: "/icons/home/levelicon/heart/inactive-pressed.png",
+  },
+  like: {
+    active_unpressed: "/icons/home/levelicon/like/active.png",
+    active_pressed: "/icons/home/levelicon/like/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/like/inactive.png",
+    inactive_pressed: "/icons/home/levelicon/like/inactive-pressed.png",
+  },
+  pencil: {
+    active_unpressed: "/icons/home/levelicon/pencil/active.png",
+    active_pressed: "/icons/home/levelicon/pencil/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/pencil/inactive.png",
+    inactive_pressed: "/icons/home/pencil/inactive-pressed.png",
+  },
+  map: {
+    active_unpressed: "/icons/home/levelicon/map/active.png",
+    active_pressed: "/icons/home/levelicon/map/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/map/inactive.png",
+    inactive_pressed: "/icons/home/levelicon/map/inactive-pressed.png",
+  },
+  person: {
+    active_unpressed: "/icons/home/levelicon/person/active.png",
+    active_pressed: "/icons/home/levelicon/person/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/person/inactive.png",
+    inactive_pressed: "/icons/home/levelicon/person/inactive-pressed.png",
+  },
+  facility: {
+    active_unpressed: "/icons/home/levelicon/facility/active.png",
+    active_pressed: "/icons/home/levelicon/facility/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/facility/inactive.png",
+    inactive_pressed: "/icons/home/levelicon/facility/inactive-pressed.png",
+  },
+  _default: {
+    active_unpressed: "/icons/home/levelicon/heart/active.png",
+    active_pressed: "/icons/home/levelicon/heart/active-pressed.png",
+    inactive_unpressed: "/icons/home/levelicon/heart/inactive.png",
+    inactive_pressed: "/icons/home/levelicon/heart/inactive-pressed.png",
+  },
+};
+
+
---
+++ frontend/src/data/MapData.js
@@ -0,0 +1,23 @@
+export const MAP_ICONS = [
+  { 
+    key: "food", 
+    label: "음식점", 
+    icon: "/icons/map/food-icon.svg" 
+  },
+  { 
+    key: "cafe", 
+    label: "카페", 
+    icon: "/icons/map/cafe-icon.svg" 
+  },
+  { 
+    key: "store", 
+    label: "편의점", 
+    icon: "/icons/map/store-icon.svg" 
+  },
+  { 
+    key: "hos", 
+    label: "병원", 
+    icon: "/icons/map/hospital-icon.svg" 
+  },
+  // ... 다른 카테고리들
+];
