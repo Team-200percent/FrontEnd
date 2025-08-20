7a349dbe68fe5d771bcaaff67e6d3f8eb4f510f0
Benzity <benzity@naver.com>
2025-08-21 07:33:32 +0900

    fix: activate locatebutton (bug fix)

--- frontend/src/components/map/LocateButton.jsx
+++ frontend/src/components/map/LocateButton.jsx
@@ -17,10 +17,41 @@ const Btn = styled.button`
   }
 `;
 
-export default function LocateButton({ onClick, disabled }) {
+export default function LocateButton({ mapRef }) {
+  const handleClick = () => {
+    if (!mapRef?.current) return;
+
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => {
+        const { kakao } = window;
+        const userLatLng = new kakao.maps.LatLng(
+          coords.latitude,
+          coords.longitude
+        );
+
+        // 바로 실행하고 이벤트 제거가 필요 없게 구현
+        mapRef.current.setLevel(4);
+        mapRef.current.relayout();
+        mapRef.current.panTo(userLatLng);
+
+        setTimeout(() => {
+          mapRef.current.relayout(); // 👈 리사이즈 재계산
+          mapRef.current.panTo(userLatLng);
+          setTimeout(() => {
+            mapRef.current.setLevel(4);
+          }, 300); // 줌은 따로
+        }, 100); // 짧은 딜레이
+      },
+      (err) => {
+        console.error("위치 접근 실패:", err);
+        alert("위치 접근을 허용해주세요!");
+      }
+    );
+  };
+
   return (
-    <Btn onClick={onClick} disabled={disabled}>
-      <img src="/icons/map/LocateIcon.svg" alt="내 위치로 이동" />
+    <Btn onClick={handleClick}>
+      <img src="/icons/map/LocateIcon.svg" alt="내 위치" />
     </Btn>
   );
 }
